3f84955ed30a7a2768bec6a4fa4082078465d2b0
Junho <51792037+ojh050118@users.noreply.github.com>
2022-07-29 23:40:56 +0900

    this 생략

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -15,7 +15,7 @@ public class PlayerAction : MonoBehaviour
 
     void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(gameObject);
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
