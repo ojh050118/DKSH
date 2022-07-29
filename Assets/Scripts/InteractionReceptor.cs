2ce85a3b1efc807cd63d7d454f96360b3e4d8f0b
Junho <51792037+ojh050118@users.noreply.github.com>
2022-07-29 16:45:41 +0900

    주석 추가

--- Assets/Scripts/InteractionReceptor.cs
+++ Assets/Scripts/InteractionReceptor.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +6,15 @@ public class InteractionReceptor : MonoBehaviour
     public GameObject InteractionTarget;
     public Text Dialog;
 
+    /// <summary>
+    /// 상호작용한 오브젝트와 상호작용을 시작합니다.
+    /// 상호작용 중에 다시 이 함수를 호출하면 상호작용을 중지합니다.
+    /// </summary>
+    /// <param name="interactionTarget">상호작용이 가능한 오브젝트.</param>
     public void Interaction(GameObject interactionTarget)
     {
+        // InteractionTarget이 null이 아닐 땐 상호작용을 하고있음을 의미합니다.
+        // 상호작용을 끝내야 하기때문에 대화상자를 비활성화하고 InteractionTarget의 값을 null로 만듭니다.
         if (InteractionTarget != null)
         {
             InteractionTarget = null;
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -21,15 +21,19 @@ public class PlayerAction : MonoBehaviour
 
     void Update()
     {
+        // 스페이스 키를 눌렀을 때 상호작용 할 대상이 있다면 그 오브젝트와 상호작용을 시작합니다.
         if (Input.GetKeyDown(KeyCode.Space) && interactionTarget != null)
             receptor.Interaction(interactionTarget);
 
+        // 상호작용 중일 때 이동 입력은 무시합니다.
         if (receptor.InteractionTarget != null)
             return;
 
         moveDelta.x = Input.GetAxisRaw("Horizontal");
         moveDelta.y = Input.GetAxisRaw("Vertical");
 
+        // 현재 입력 값으로 플레이어의 방향을 결정합니다.
+        // Todo: 대각선 방향으로 이동하는 것을 허용하지 않아야합니다.
         if (moveDelta.x != 0)
             currentDirection = moveDelta.x < 0 ? Vector3.left : Vector3.right;
 
@@ -39,12 +43,18 @@ public class PlayerAction : MonoBehaviour
 
     void FixedUpdate()
     {
+        // 애니메이션 매니저의 매개변수에 값을 설정합니다.
+        // 애니메이션 매니저는 매개변수 값이 변하는 즉시 애니메이션을 실행합니다.
+        // 일정하게 애니메이션을 실행해야 하기때문에 FixedUpdate() 함수에서 실행해야합니다.
         processAnimation();
 
         rigid.velocity = moveDelta * Speed;
 
+        // 플레이어 전방에 오브젝트가 있는지 확인하기 위해 플레이어의 위치에서 하나의 선을 만듭니다.
+        // 오브젝트는 Interactonable레이어에 있는 오브젝트로 한정됩니다.
         var rayHit = Physics2D.Raycast(rigid.position, currentDirection, 0.7f, LayerMask.GetMask("Interactionable"));
 
+        // 선과 충돌한 오브젝트가 있으면 그것은 상호작용이 가능한 오브젝트입니다.
         if (rayHit.collider != null)
             interactionTarget = rayHit.collider.gameObject;
         else
