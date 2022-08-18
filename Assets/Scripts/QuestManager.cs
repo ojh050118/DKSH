ad9673991bb4a190b3f31e8c843370892cef672d
cocoa-dev-1 <sinmounbu0510@naver.com>
2022-08-18 21:20:54 +0900

    feat: 매점 퀘스트 finish

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -5,88 +5,82 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
-    public static int questId;
-    public static int questActionIndex;
-    public GameObject[] questObject;
+  public static int questId;
+  public static int questActionIndex;
+  public GameObject[] questObject;
 
-    public TextAsset QuestDataJson;
+  public TextAsset QuestDataJson;
 
-    Dictionary<int, QuestData> questList;
+  Dictionary<int, QuestData> questList;
 
-    void Awake()
+  void Awake()
+  {
+    questList = parseQuestData(QuestDataJson.text);
+    if (questId == 0) questId = 10;
+  }
+
+  public int GetQuestTalkIndex(int id)
+  {
+    return questId + questActionIndex;
+  }
+
+  public string CheckQuest(int id)
+  {
+
+    //Next Talk Object
+    if (id == questList[questId].npcId[questActionIndex])
+      questActionIndex++;
+
+    //Control Quest Object
+    ControlObject();
+
+    //Talk Complete & Next Quest
+    if (questActionIndex == questList[questId].npcId.Length)
     {
-        questList = parseQuestData(QuestDataJson.text);
-        if (questId == 0) questId = 10;
+      NextQuest();
     }
 
-    public int GetQuestTalkIndex(int id)
+    //Return Quest Name
+    return questList[questId].questName;
+  }
+
+  public string CheckQuest()
+  {
+    //Return Quest Name
+    return questList[questId].questName;
+  }
+
+  void NextQuest()
+  {
+    questId += 10;
+    questActionIndex = 0;
+  }
+
+  void ControlObject()
+  {
+    switch (questId)
     {
-        return questId + questActionIndex;
-    }
-
-    public string CheckQuest(int id)
-    {
-
-        //Next Talk Object
-        if (id == questList[questId].npcId[questActionIndex])
-            questActionIndex++;
-
-        //Control Quest Object
-        ControlObject();
-
-        //Talk Complete & Next Quest
-        if (questActionIndex == questList[questId].npcId.Length)
+      case 20:
+        if (questActionIndex == 1)
         {
-            NextQuest();
+          questObject[0].SetActive(false);
         }
-
-        //Return Quest Name
-        return questList[questId].questName;
+        break;
     }
+  }
 
-    public string CheckQuest()
+  Dictionary<int, QuestData> parseQuestData(string json)
+  {
+    var questData = JsonConvert.DeserializeObject<QuestList[]>(json, new JsonSerializerSettings
     {
-        //Return Quest Name
-        return questList[questId].questName;
-    }
+      Formatting = Formatting.Indented
+    });
 
-    void NextQuest()
-    {
-        questId += 10;
-        questActionIndex = 0;
-    }
+    Dictionary<int, QuestData> result = new Dictionary<int, QuestData>();
 
-    void ControlObject()
-    {
-        //switch(questId)
-        //{
-        //    case 10:
-        //        if (questActionIndex == 2)
-        //        {
-        //            questObject[0].SetActive(true);
-        //        }
-        //        break;
-        //    case 20:
-        //        if (questActionIndex == 1)
-        //        {
-        //            questObject[0].SetActive(false);
-        //        }
-        //        break;
-        //}
-    }
+    foreach (var data in questData)
+      result.Add(data.Id, data.quest);
 
-    Dictionary<int, QuestData> parseQuestData(string json)
-    {
-        var questData = JsonConvert.DeserializeObject<QuestList[]>(json, new JsonSerializerSettings
-        {
-            Formatting = Formatting.Indented
-        });
-
-        Dictionary<int, QuestData> result = new Dictionary<int, QuestData>();
-
-        foreach (var data in questData)
-            result.Add(data.Id, data.quest);
-
-        return result;
-    }
+    return result;
+  }
 }
